3765f072b70bef480c916e22b7d50fcf60b54a66
Andrew Finsand <andfinsand@gmail.com>
2024-02-27 12:49:47 -0800

    Responsive.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -28,11 +28,11 @@ const Banner = () => {
             <div id='banner' className='flex justify-center relative rounded-3xl w-full h-[1000px]'>
 
                 {/* Quote */}
-                <div className='self-center flex flex-col text-center text-lg text-white font-poppinsMedium gap-8 z-10 px-16 sm:text-2xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>
+                <div className='self-center flex flex-col text-center text-lg text-white font-poppinsMedium z-10 px-5 md:px-16 xl:gap-8'>
                     <div
                         data-aos="fade-in"
                         data-aos-once="true"
-                        className='gradient-text text-7xl font-poppinsBold'
+                        className='gradient-text text-2xl font-poppinsBold sm:text-4xl lg:text-5xl xl:text-7xl'
                         >
                         Developing the tools today...
                     </div>
@@ -40,7 +40,7 @@ const Banner = () => {
                         data-aos="fade-in"
                         data-aos-delay="1300"
                         data-aos-once="true"
-                        className='text-white text-4xl'>that will shape our tomorrow
+                        className='text-white text-base sm:text-2xl lg:text-3xl xl:text-4xl'>that will shape our tomorrow
                     </div>
                 </div>
             </div>
--- src/components/Center.tsx
+++ src/components/Center.tsx
@@ -5,18 +5,18 @@ import ProjectCard from './ProjectCard'
 const Center = () => {
     return (
         <>
-            <div className=' flex flex-col justify-center bg-[#EFEFEF] backdrop-blur-3xl h-fit px-14 py-32'>
+            <div className=' flex flex-col justify-center bg-[#EFEFEF] backdrop-blur-3xl h-fit px-5 py-32 md:px-14'>
             <Banner />
             <div className='flex flex-col'>
-                <div className='flex flex-col sm:flex sm:flex-row justify-between mt-32'>
+                <div className='flex flex-col justify-between gap-16 sm:flex sm:flex-row mt-32'>
                     <ProjectCard />
                     <ProjectCard />
                 </div>
-                <div className='flex flex-col sm:flex sm:flex-row justify-between mt-16'>
+                <div className='flex flex-col justify-between gap-16 sm:flex sm:flex-row mt-16'>
                     <ProjectCard />
                     <ProjectCard />
                 </div>
-                <div className='flex flex-col sm:flex sm:flex-row justify-between mt-16'>
+                <div className='flex flex-col justify-between gap-16 sm:flex sm:flex-row mt-16'>
                     <ProjectCard />
                     <ProjectCard />
                 </div>
