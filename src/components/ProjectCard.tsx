870cbf680cf43e45aa59fb5c5b1d3300b4b07cfc
Andrew Finsand <andfinsand@gmail.com>
2024-03-02 11:19:16 -0800

    CSS change height of project footer from 15% to 100%. Project Cards added function to increase footer height. Additional div wrapping footer with button states. Addtiionaly div below button for slide. And state styling for button.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,7 +5,21 @@ const ProjectCard= () => {
     const [isHovered, setIsHovered] = useState(false);
     const [isVideoPlaying, setIsVideoPlaying] = useState(false);
     const videoRef = useRef<any>(null);
-    const [displayContent, setDisplayContent] = useState<boolean>(false);
+    const [showSlide, setShowSlide] = useState<boolean>(false);
+
+    const [slideHeight, setSlideHeight] = useState('15%');
+    const [footerHeight, setFooterHeight] = useState('100%');
+
+    const toggleFooter = () => {
+        setShowSlide(!showSlide);
+        if(!showSlide) {
+            setSlideHeight('100%');
+            setFooterHeight('15%'); // SET THE project-footer DIV TO THIS AS A STYLE TO MAKE THE FOOTER STICK TO THE TOP
+        } else {
+            setSlideHeight('15%');
+            setFooterHeight('100%');
+        }
+    }
 
     // Play video on hover and pause when hover is stopped
     const handleMouseEnter = () => {
@@ -75,44 +89,53 @@ const ProjectCard= () => {
                 </a>
 
                 {/* Footer on hover */}
-                <div className='project-footer absolute bottom-0 bg-projectFooter backdrop-blur-lg rounded-b-3xl text-white z-10 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out'>
-                    <div className='flex justify-between items-center h-full px-5 lg:px-7 opacity-0 group-hover:opacity-100 transition-opacity duration-100 ease-in-out'>
+                <div style={{ height: slideHeight }}  className={`absolute overflow-hidden bottom-0 bg-transparent w-full ${showSlide ? ' rounded-3xl' : ''}`}>
 
-                        {/* Project name */}
-                        <a
-                            href='https://pixelangelo.io/'
-                            target='_blank'
-                            className='tracking-wide hover:text-[#363636] duration-100 lg:font-poppinsMedium 2xl:text-xl'
-                        >
-                            PixelAngelo
-                        </a>
+                    <div   className={`project-footer absolute h-full bottom-0 bg-projectFooter backdrop-blur-lg text-white z-20 w-full opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out ${showSlide ? 'rounded-t-3xl rounded-b-0 opacity-100 top-0 h-[15%]' : 'rounded-t-0 rounded-b-3xl'}`}>
+                        <div className={`flex justify-between items-center h-full px-5 lg:px-7  ${showSlide ? 'opacity-0 group-hover:opacity-100 transition-opacity duration-100 ease-in-out' : ''}`}>
 
-                        <div className='flex gap-6'>
-
-                            {/* Github icon */}
+                            {/* Project name */}
                             <a
-                                href='https://github.com/andfinsand/PixelAngelo'
+                                href='https://pixelangelo.io/'
                                 target='_blank'
+                                className='tracking-wide hover:text-[#363636] duration-100 lg:font-poppinsMedium 2xl:text-xl'
                             >
-                                <Image
-                                    src='/github-project.svg'
-                                    alt='Github logo'
-                                    width={100}
-                                    height={100}
-                                    priority={true}
-                                    className='hover:brightness-[.25] duration-100 w-6 2xl:w-8'
-                                />
+                                PixelAngelo
                             </a>
 
-                            {/* Info button to display project slide */}
-                            <button
-                                onClick={() => setDisplayContent(!displayContent)}
-                                className='flex justify-center bg-white/40 hover:bg-black/30 duration-100 rounded-[4px] w-6 2xl:w-8'>
-                                <span className={`caret self-center ${displayContent ? 'transform rotate-180 duration-200' : 'transform rotate-0 duration-200'}`}></span>
-                            </button>
-                        </div>
+                            <div className='flex gap-6'>
 
+                                {/* Github icon */}
+                                <a
+                                    href='https://github.com/andfinsand/PixelAngelo'
+                                    target='_blank'
+                                >
+                                    <Image
+                                        src='/github-project.svg'
+                                        alt='Github logo'
+                                        width={100}
+                                        height={100}
+                                        priority={true}
+                                        className='hover:brightness-[.25] duration-100 w-6 2xl:w-8'
+                                    />
+                                </a>
+
+                                {/* Info button to display project slide */}
+                                <button
+                                    onClick={toggleFooter}
+                                    className='flex justify-center bg-white/40 hover:bg-black/30 duration-100 rounded-[4px] w-6 2xl:w-8'>
+                                    <span className={`caret self-center ${showSlide ? 'transform rotate-180 duration-200' : 'transform rotate-0 duration-200'}`}></span>
+                                </button>
+                            </div>
+
+                        </div>
                     </div>
+
+                    {/* Make this a component for the slide info */}
+                    <div  className={`absolute bottom-0 h-full w-full ${showSlide ? 'opacity-100 bg-transparent bg-blue-300 rounded-3xl' : 'bg-white opacity-0 duration-200'}`}>
+                        Project content etc.
+                    </div>
+
                 </div>
 
             </div>
