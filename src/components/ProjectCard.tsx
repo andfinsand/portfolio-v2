1316b7f54aa651259920f0a0c51b7923ca6635d5
Andrew Finsand <andfinsand@gmail.com>
2024-03-15 16:50:51 -0700

    Trigger footer animation on click for mobile instead of hover.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -21,6 +21,7 @@ const ProjectCard= ({ name, thumbnail, demo, projectUrl, github, description, te
     const [showSlide, setShowSlide] = useState<boolean>(false);
     const [showVideo, setShowVideo] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
 
     // Detect device type, mobile vs desktop
     useEffect(() => {
@@ -129,14 +130,55 @@ const ProjectCard= ({ name, thumbnail, demo, projectUrl, github, description, te
                     {/* Footer */}
                     {isMobile ? (
 
-                            <button
-                                onClick={toggleSlider}
-                                className='flex justify-center bg-white/40 hover:bg-black/30 duration-100 rounded-[4px] w-6 h-[23.5px] 2xl:w-8 2xl:h-[31.3px]'>
-                                <span className='caret self-center'></span>
-                            </button>
+                        <div
+                        style={isClicked ? { height: '15%', transition: 'all 0.2s' } : { height: '0%', transition: 'all 0.2s' }}
+                        className='absolute h-full bottom-0 bg-projectFooter backdrop-blur-lg rounded-b-3xl text-white w-full z-30 opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out'
+                        >
+
+                        {/* Wrapper for name, github, and button */}
+                        <div className='flex justify-between items-center h-full opacity-0 group-hover:opacity-100 transition-opacity duration-100 ease-in-out px-5 lg:px-7'>
+
+                            {/* Project name */}
+                            <a
+                                href={projectUrl}
+                                target='_blank'
+                                className='tracking-wide duration-100 hover:text-[#363636] lg:font-poppinsMedium 2xl:text-xl'
+                            >
+                                {name}
+                            </a>
+
+                            <div className='flex gap-6'>
+
+                                {/* Github icon */}
+                                {github && (
+                                    <a
+                                        href={github}
+                                        target='_blank'
+                                    >
+                                        <Image
+                                            src='/github-project.svg'
+                                            alt='Github logo'
+                                            width={100}
+                                            height={100}
+                                            priority={true}
+                                            className='w-6 hover:brightness-[.25] duration-100 2xl:w-8'
+                                        />
+                                    </a>
+                                )}
+
+                                {/* Info button to display project slide */}
+                                <button
+                                    onClick={toggleSlider}
+                                    className='flex justify-center bg-white/40 hover:bg-black/30 duration-100 rounded-[4px] w-6 h-[23.5px] 2xl:w-8 2xl:h-[31.3px]'>
+                                    <span className='caret self-center'></span>
+                                </button>
+                            </div>
+                        </div>
+                        </div>
 
                         ) : (
 
+                    // Desktop
                     <div
                         style={projectFooter}
                         className='absolute h-full bottom-0 bg-projectFooter backdrop-blur-lg rounded-b-3xl text-white w-full z-30 opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out'
