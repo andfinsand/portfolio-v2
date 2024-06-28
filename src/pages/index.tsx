ecce42d837f90c926f5bf53bdcb31e45628838c0
Andrew Finsand <andfinsand@gmail.com>
2024-06-27 17:08:01 -0700

    Update meta tags to improve SEO.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,14 @@ export default function Home() {
 
       {/* Metadata */}
       <Head>
-        <title>Andrew Finsands Portfolio</title>
-        <meta name='description' content='Andrew Finsands software developer portfolio' />
-        <meta name='keywords' content='Andrew Finsand, Developer, Python, Django, Flask, React, Next.js, MySQL, Javascript, HTML5, CSS3, Tailwind, APIs, Figma, Docker, AI, design' />
+        <title>Andrew Finsand | Software Developer Portfolio</title>
+        <meta name='description' content="Andrew Finsand's software developer portfolio showcasing expertise in Next.js, Python, Design and more. View projects and skills." />
+        <meta name='keywords' content='Andrew Finsand, Andrew, Finsand, Andrew Oscar Finsand, Developer, Python, Django, Flask, React, Next.js, MySQL, Javascript, HTML5, CSS3, Tailwind, APIs, Figma, Docker, AI, design' />
         <meta name='author' content='Andrew Finsand' />
-        <meta name='viewport' content='width=device-width' />
-        <meta name="google-site-verification" content="NaDSZfAj1DbjfdG6_tgPr7XvX2ar9z36JMEQPvjkcds" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='google-site-verification' content='NaDSZfAj1DbjfdG6_tgPr7XvX2ar9z36JMEQPvjkcds' />
+        <link rel='canonical' href='https://www.andrewfinsand.dev' />
+        <link rel='icon' href='/favicon.ico' />
         {/* Preload background images */}
         <link rel='preload' href='/background-sky-green.jpg' as='image' />
         <link rel='preload' href='/foreground.webp' as='image' />
