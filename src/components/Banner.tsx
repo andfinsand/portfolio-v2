83bba069ffec506f7f82bfc65275acd18bea7f79
Andrew Finsand <andfinsand@gmail.com>
2024-07-15 15:32:34 -0700

    Tighter letter tracking for banner text.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -25,16 +25,16 @@ const Banner = () => {
             <div id='banner' className='flex justify-center relative rounded-3xl w-full h-[1000px]'>
 
                 {/* Quote */}
-                <div className='flex flex-col self-center text-center text-lg text-white font-poppinsMedium z-10 px-5 md:px-16 sm:gap-1 lg:gap-3 xl:gap-7'>
+                <div className='flex flex-col self-center text-center text-lg text-white font-poppinsMedium z-10 px-5 md:px-16 sm:tracking-widest sm:gap-1 lg:gap-3 xl:gap-7'>
                     <div
                         data-aos='fade-in'
                         data-aos-once='true'
                         className='text-xl sm:text-3xl lg:text-4xl xl:text-5xl 4xl:text-6xl'
                     >
-                        <span className='gradient-text text-2xl tracking-wide font-poppinsBlack sm:text-4xl lg:text-5xl xl:text-7xl 4xl:text-8xl'>Developing </span>
+                        <span className='gradient-text text-2xl tracking-tighter font-poppinsBlack sm:pr-1 sm:text-4xl lg:text-5xl xl:pr-1.5 xl:text-7xl 4xl:pr-2 4xl:text-8xl'>Developing </span>
                         <span data-aos='fade-in' data-aos-once='true' data-aos-delay='1000'>
                             the
-                            <span className='gradient-text text-2xl tracking-wide font-poppinsBlack sm:text-4xl lg:text-5xl xl:text-7xl 4xl:text-8xl'> tools </span>
+                            <span className='gradient-text text-2xl tracking-tighter font-poppinsBlack sm:pr-1 sm:text-4xl lg:text-5xl xl:pr-1.5 xl:text-7xl 4xl:pr-2 4xl:text-8xl'> tools </span>
                         </span>
                         <span data-aos='fade-in' data-aos-once='true' data-aos-delay='2000'>today...</span>
                     </div>
@@ -46,7 +46,7 @@ const Banner = () => {
                         className='text-white text-sm sm:text-xl lg:text-2xl xl:text-4xl 4xl:text-5xl'
                     >
                         that will
-                        <span className='gradient-text text-xl tracking-wide font-poppinsBlack sm:text-3xl lg:text-4xl xl:text-5xl 4xl:text-6xl'> shape </span>
+                        <span className='gradient-text text-xl tracking-tighter font-poppinsBlack sm:pr-1 sm:text-3xl lg:text-4xl xl:pr-1.5 xl:text-5xl 4xl:pr-2 4xl:text-6xl'> shape </span>
                         <span data-aos='fade-in' data-aos-once='true' data-aos-delay='4000'>our tomorrow</span>
                     </div>
                 </div>
