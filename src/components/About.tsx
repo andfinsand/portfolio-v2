81411a0d591d3608bc0ba94066c3a4eed272dcb2
Andrew Finsand <andfinsand@gmail.com>
2025-03-27 15:53:47 -0700

    Update new headshot and change about section background

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,24 +18,24 @@ const About = () => {
 
     // Background parallax effect
     useEffect(() => {
-        document.addEventListener('scroll', function() {
+        document.addEventListener('scroll', function () {
             let scrollPosition = window.scrollY;
             let parallaxElement = document.getElementById('about');
             let speed = 0.5;
 
             if (parallaxElement) {
-            parallaxElement.style.backgroundPosition = `center ${-scrollPosition * speed}px`;
-        }
-    });
+                parallaxElement.style.backgroundPosition = `center ${-scrollPosition * speed}px`;
+            }
+        });
 
-    return () => {
-        document.removeEventListener('scroll', function() {});
-    };
+        return () => {
+            document.removeEventListener('scroll', function () { });
+        };
     }, []);
 
     return (
         <>
-            <div id='about' className='flex justify-center items-center min-h-[850px] rounded-3xl mt-28 md:py-6 md:mt-32 lg:py-20 lg:h-fit lg:min-h-fit xl:h-[850px] 3xl:min-h-[580px]'>
+            <div id='about' className='flex justify-center items-center min-h-[850px] rounded-3xl mt-28 md:py-6 md:mt-32 lg:py-20 lg:h-fit xl:h-[850px] 3xl:min-h-[580px]'>
 
                 {/* About container including headshot */}
                 <div className='flex flex-col justify-center items-center rounded-3xl text-white w-11/12 max-w-[1250px] h-4/5 min-h-[1000px] gap-8 lg:h-[385px] lg:px-14 xl:flex xl:flex-row xl:justify-between 2xl:max-w-[1350px] 3xl:w-11/12 3xl:max-w-[1550px] 3xl:h-4/6 3xl:max-h-[550px] 3xl:min-h-[490px]'>
@@ -117,7 +117,7 @@ const About = () => {
                         data-aos-once='true'
                         data-aos-offset='200'
 
-                        src='/headshot-mobile.webp'
+                        src='/headshot2x.webp'
                         alt='Headshot of Andrew Finsand'
                         width={897}
                         height={897}
